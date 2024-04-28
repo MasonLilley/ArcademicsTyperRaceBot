d67067bc275d0faa3859e3cf2fa2bf204847ea04
mason.lilley <mason.lilley@gmail.com>
2024-04-28 18:06:34 -0500

    yuh

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pyautogui
 import pytesseract
 import time
+import random
+import numpy as np
 
 import masonConstents as c
 # import maxConstents as c
 
+custom_config = r'--oem 1 --psm 6 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz'
+
 def capture_screen(region):
     return pyautogui.screenshot(region=region)
 
+def compare_images(img1, img2):
+    arr1 = np.array(img1)
+    arr2 = np.array(img2)
+    
+    if np.array_equal(arr1, arr2):
+        return True
+    else:
+        return False
+
 def solve_current_question():
-    ss = capture_screen(c.Region1)
-    ss.save("screenshot.png")
-    textToType = pytesseract.image_to_string(ss)
-    pyautogui.write(textToType)
+    ss = capture_screen(c.Region1).convert('L')
+    ss.save("ss.png")
+    oldSS.save("oldSS.png")
+    if (not(compare_images(oldSS, ss))):
+        textToType = pytesseract.image_to_string(ss, config=custom_config)
+        print(textToType)
+        pyautogui.typewrite(textToType, interval=0.0125)
+        time.sleep(0.1)
+        oldSS == ss
+oldSS = capture_screen(c.Region1).convert('L')
 
 while True:
     # pyautogui.displayMousePosition()
     solve_current_question()
-
--- masonConstents.py
+++ masonConstents.py
@@ -1,6 +1,12 @@
 #first number
-topLeftXfirst = 675
-topLeftYfirst = 610
-bottomRightXfirst = 1032
-bottomRightYfirst = 670
+topLeftXfirst = 780
+topLeftYfirst = 600
+bottomRightXfirst = 1100
+bottomRightYfirst = 675
+#second number
+topLeftX2 = 676
+topLeftY2 = 609
+bottomRightX2 = 1033
+bottomRightY2 = 669
 Region1 = (topLeftXfirst, topLeftYfirst, (bottomRightXfirst-topLeftXfirst), (bottomRightYfirst-topLeftYfirst))
+Region2 = (topLeftX2, topLeftY2, (bottomRightX2-topLeftX2), (bottomRightY2-topLeftY2))
